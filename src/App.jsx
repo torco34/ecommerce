1ada60d717b3be1b6992eda7641853c88ff5f924
torcoroma <torcoromaarias@gmail.com>
2023-02-22 16:28:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Home } from "./pages/Home"
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <h1>ecomerc</h1>
+    <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { HomCarousel } from "../../components/HomCarousel"
+
+export function Home() {
+    return(
+        <>
+        <h1>home</h1>
+        <HomCarousel/>
+        </>
+        
+    )
+}
